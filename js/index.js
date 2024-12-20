eb157cb2f0ea8dcd47d1d7de22d67bed3741facb
jsnklmr <91213802+jsnklmr@users.noreply.github.com>
2024-12-19 22:32:31 -0500

    experimented with variables

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+
+const one = 1;
+console.log(one);
+
+let bestFriend = "Joe";
+console.log(bestFriend);
+
+bestFriend = "Susan";
+console.log(bestFriend);
+
+const one = 2;
+console.log(one);
+
+let bestFriend = "Jim";
+console.log(bestFriend);
